1a60cb8f7227ef9082c9abdeca942e1c293eb422
GhostDrew11 <armelhell@gmail.com>
2025-07-21 17:40:34 -0400

    Day 3 useGameTimer

--- src/hooks/useGameTimer.ts
+++ src/hooks/useGameTimer.ts
@@ -1,37 +1,31 @@
 import { useCallback, useEffect, useState } from "react";
 
 export const useGameTimer = (
-  initialTimer: number,
+  initialDuration: number,
   isActive: boolean,
   onTimeUp: () => void
 ) => {
-  const [timeRemaining, setTimeRemaining] = useState(initialTimer);
-  const [isPaused, setIsPaused] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(initialDuration);
 
-  //
   useEffect(() => {
-    if (!isActive || isPaused) return;
-
-    if (timeRemaining <= 0) {
-      setTimeRemaining(0); // ensure no more negative countdown
-      onTimeUp();
-      return;
-    }
+    if (!isActive || timeRemaining <= 0) return;
 
     const timer = setInterval(() => {
-      setTimeRemaining((prev) => prev - 1);
+      setTimeRemaining((prev) => {
+        if (prev <= 1) {
+          onTimeUp();
+          return 0;
+        }
+        return prev - 1;
+      });
     }, 1000);
 
     return () => clearInterval(timer);
-  }, [timeRemaining, isActive, onTimeUp, isPaused]);
+  }, [timeRemaining, isActive, onTimeUp]);
 
   const resetTimer = useCallback((newTime: number) => {
     setTimeRemaining(newTime);
-    setIsPaused(false);
   }, []);
 
-  const pauseTimer = useCallback(() => setIsPaused(true), []);
-  const resumeTimer = useCallback(() => setIsPaused(false), []);
-
-  return { timeRemaining, resetTimer, pauseTimer, resumeTimer, isPaused };
+  return { timeRemaining, resetTimer };
 };
